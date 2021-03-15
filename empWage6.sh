09c98dec09bd2d65618a38acb578354891590c4e
kishanRaj22 <kishanraj22479@gmail.com>
2021-03-15 21:31:24 +0530

    ADDED: calculate wage using function

---
+++ empWage6.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Calculate wages hours and day using function"
+
+PER_HOUR=20
+FULL_DAY_HOUR=8
+PART_TIME_HOUR=4
+NUMBER_OF_HOUR=100
+NUMBER_OF_DAYS=20
+#variable
+numberOfDays=20
+totalWorkingDays=0
+totalEmpHrs=0
+#Using function
+function getWorkingHours()
+{
+	random=$1											#Using case statement
+	case $random in
+		1)
+			empHrs=8						 #Calcute full time
+			;;
+
+		2)
+			empHrs=4						 #Calculate part time
+			;;
+
+		3)
+			empHrs=0
+			;;
+	esac
+echo "$empHrs"
+}
+while(( $NUMBER_OF_HOUR > $totalEmpHrs  && $NUMBER_OF_DAYS > $totalWorkingDays ))
+do
+   totalWorkingDays=$((totalWorkingDays+1))
+   empHrs="$( getWorkingHours $((RANDOM%3+1)) )"
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))	# Total employee hour
+done
+totalSalary=$(($totalEmpHrs*$numberOfDays))
+echo "Total Salary is : $totalSalary"
